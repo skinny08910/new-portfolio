044c3f040bfd740b8ff9cb54334fd9a262e6fc38
Nicksonder Examar <nextgendev12@gmail.com>
2021-05-14 01:19:45 -0400

    Finished refactoring the site, I'm going to deploy it now.

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,6 @@ $(function ($) {
   "use strict";
 
   jQuery(document).ready(function () {
-
     $(".toggle-icon").on("click", function () {
       $(".my-navbar").toggleClass("show");
       $(".mainmenu-area").toggleClass("show");
@@ -26,7 +25,7 @@ $(function ($) {
 
     $("#home .pagelink").on("click", function (e) {
       e.preventDefault();
-      if (e.target.parentNode.id = "g-p-f-h") {
+      if ((e.target.parentNode.id = "g-p-f-h")) {
         $(".my-navbar .mynav-item").removeClass("active");
         $(".my-navbar .mynav-item.portfolio").addClass("active");
         $(".my-navbar .mynav-item .portfolio").addClass("active");
@@ -41,7 +40,7 @@ $(function ($) {
     var $typed = $("#typed");
     if ($typed.length > 0) {
       $typed.typed({
-        strings: ["Web Design", "Web Development", "UI/UX Design"],
+        strings: ["SEO", "Email Marketing", "Web Development"],
         stringsElement: null,
         typeSpeed: 70,
         startDelay: 150,
@@ -73,10 +72,10 @@ $(function ($) {
       },
     });
 
-    // Ajax On Modal 
-    $(".service-area-wrapper").each(function () {
+    // Ajax On Modal
+    $(".project-gallery-item").each(function () {
       $(this).magnificPopup({
-        delegate: ".service-modal:visible",
+        delegate: ".pp:visible",
         type: "ajax",
         tLoading:
           '<div class="preloader"><div class="lds-ellipsis"><div></div><div></div><div></div><div></div></div></div>',
@@ -86,64 +85,50 @@ $(function ($) {
         gallery: {
           enabled: true,
         },
-      });
-    });
-    $('.project-gallery-item').each(function() {
-      $(this).magnificPopup({
-        delegate: '.pp:visible',
-          type: "ajax",
-        tLoading: '<div class="preloader"><div class="lds-ellipsis"><div></div><div></div><div></div><div></div></div></div>',
-        mainClass: "mfp-fade",
-        closeBtnInside: true,
-        midClick: true,
-        gallery: {
-            enabled: true,
-          },
         callbacks: {
-          ajaxContentAdded: function() {
+          ajaxContentAdded: function () {
             $(".owl-carousel").each(function (index) {
               var a = $(this);
               $(this).owlCarousel({
-              autoplay: a.data('autoplay'),
-              center: a.data('center'),
-              autoplayTimeout: a.data('autoplaytimeout'),
-              autoplayHoverPause: a.data('autoplayhoverpause'),
-              loop: a.data('loop'),
-              speed: a.data('speed'),
-              nav: a.data('nav'),
-              dots: a.data('dots'),
-              autoHeight: a.data('autoheight'),
-              autoWidth: a.data('autowidth'),
-              margin: a.data('margin'),
-              stagePadding: a.data('stagepadding'),
-              slideBy: a.data('slideby'),
-              lazyLoad: a.data('lazyload'),
-              navText:['<i class="fa fa-chevron-left"></i>', '<i class="fa fa-chevron-right"></i>'],
-              animateOut: a.data('animateOut'),
-              animateIn: a.data('animateIn'),
-              video: a.data('video'),
-              items: a.data('items'),
-              responsive:{
-                0:{items: a.data('items-xs'),},
-                576:{items: a.data('items-sm'),},
-                768:{items: a.data('items-md'),},
-                992:{items: a.data('items-lg'),}
-              }	
-                      });
-                  });
-              }
-          }
+                autoplay: a.data("autoplay"),
+                center: a.data("center"),
+                autoplayTimeout: a.data("autoplaytimeout"),
+                autoplayHoverPause: a.data("autoplayhoverpause"),
+                loop: a.data("loop"),
+                speed: a.data("speed"),
+                nav: a.data("nav"),
+                dots: a.data("dots"),
+                autoHeight: a.data("autoheight"),
+                autoWidth: a.data("autowidth"),
+                margin: a.data("margin"),
+                stagePadding: a.data("stagepadding"),
+                slideBy: a.data("slideby"),
+                lazyLoad: a.data("lazyload"),
+                navText: [
+                  '<i class="fa fa-chevron-left"></i>',
+                  '<i class="fa fa-chevron-right"></i>',
+                ],
+                animateOut: a.data("animateOut"),
+                animateIn: a.data("animateIn"),
+                video: a.data("video"),
+                items: a.data("items"),
+                responsive: {
+                  0: { items: a.data("items-xs") },
+                  576: { items: a.data("items-sm") },
+                  768: { items: a.data("items-md") },
+                  992: { items: a.data("items-lg") },
+                },
+              });
+            });
+          },
+        },
       });
-      });
-});
-
-  $(window).on('load', function () {
-    /*Preloader*/
-    var preLoder = $("#preloader");
-    preLoder.addClass('hide');
+    });
   });
 
-
-
-
+  $(window).on("load", function () {
+    /*Preloader*/
+    var preLoder = $("#preloader");
+    preLoder.addClass("hide");
+  });
 });
